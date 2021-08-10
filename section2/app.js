ca28af1a88936e7408a6501d27fb9c1da0725b50
Adam-Harte <adamharte11@hotmail.co.uk>
2021-08-10 13:40:19 +0100

    assignment 7 complete adding get and update status api handlers

--- section2/app.js
+++ section2/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
+const statusRoutes = require('./routes/status');
 
 const app = express();
 
@@ -44,6 +45,7 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
+app.use('/status', statusRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
